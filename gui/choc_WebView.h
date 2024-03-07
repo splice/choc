896796120317c70ebb015e360fdeb633d2a979d7
Alex Wood <awood314@gmail.com>
2024-03-07 09:40:08 -0800

    disable web security

--- gui/choc_WebView.h
+++ gui/choc_WebView.h
@@ -348,6 +348,7 @@ struct choc::ui::WebView::Pimpl
         call<id> (prefs, "setValue:forKey:", getNSNumberBool (true), getNSString ("fullScreenEnabled"));
         call<id> (prefs, "setValue:forKey:", getNSNumberBool (true), getNSString ("DOMPasteAllowed"));
         call<id> (prefs, "setValue:forKey:", getNSNumberBool (true), getNSString ("javaScriptCanAccessClipboard"));
+        call<id> (prefs, "setValue:forKey:", getNSNumberBool (false), getNSString ("webSecurityEnabled"));
 
         if (options.enableDebugMode)
             call<id> (prefs, "setValue:forKey:", getNSNumberBool (true), getNSString ("developerExtrasEnabled"));
